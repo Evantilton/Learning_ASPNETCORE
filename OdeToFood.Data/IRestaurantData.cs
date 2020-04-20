0abfe9860887cee48045c54b8829b28cf3a48291
evantilton <evantilton@gmail.com>
2020-04-20 12:04:08 -0500

    added some hardcoded data and references

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData: IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurants>()
+            {
+                new Restaurant { ID = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine=CuisineType.Italian},
+            }
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
